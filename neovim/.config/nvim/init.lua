0639c09548a084ecb63c363ff3b4f720a562324d
Flowrey <flowrey@laposte.net>
2022-09-29 17:45:49 +0200

    add telescope plugin

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -9,6 +9,7 @@ require('packer').startup(function()
     use 'tpope/vim-surround'
     use 'tpope/vim-fugitive'
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { {'nvim-lua/plenary.nvim'} } }
 end)
 
 vim.g.gruvbox_italic = 1
@@ -31,6 +32,7 @@ vim.opt.background = "dark"
 vim.cmd 'colorscheme gruvbox'
 vim.cmd 'syntax enable'
 vim.cmd 'filetype plugin indent on'
+
 
 local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
