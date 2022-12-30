e283d14ddf3a0ae94254b7bed17a1861e25afb26
Flowrey <flowrey@laposte.net>
2022-12-30 16:38:13 +0100

    Refactor

--- bittorent/src/lib.rs
+++ bittorent/src/lib.rs
@@ -11,11 +11,8 @@
 //! <https://www.bittorrent.org/beps/bep_0003.html>
 
 use sha1::{Digest, Sha1};
-use std::fs::File;
 use std::io::prelude::*;
-use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
-use url::Url;
-
+use std::net::{SocketAddrV4, TcpStream};
 pub mod handshake;
 pub mod message;
 pub mod metainfo;
@@ -25,158 +22,82 @@ pub mod utils;
 use crate::handshake::Handshake;
 use crate::message::Message;
 use crate::metainfo::Metainfo;
-use crate::utils::urlencode;
 
-impl<'a> Metainfo<'a> {
-    #[allow(dead_code)]
-    pub fn from_bytes(bytes: &'a [u8]) -> Self {
-        bendy::serde::from_bytes::<Metainfo>(&bytes).expect("Failed to deserialize torrent file")
+pub struct Peer {
+    _socket_addr_v4: SocketAddrV4,
+    metainfo: Metainfo,
+    stream: TcpStream,
+}
+
+impl Peer {
+    pub fn establish_connection(socket_addr_v4: SocketAddrV4, metainfo: Metainfo) -> Result<Peer, String> {
+        Ok(Peer {
+            _socket_addr_v4: socket_addr_v4,
+            metainfo,
+            stream: TcpStream::connect(socket_addr_v4)
+                .map_err(|_| "Couldn't connect to peer")?,
+        })
     }
 
-    pub fn get_info_hash(&self) -> [u8; 20] {
-        let mut hasher = Sha1::new();
-        let bencoded_info = bendy::serde::to_bytes(&self.info).expect("Failed to encode info");
-        hasher.update(bencoded_info);
-        hasher
-            .finalize()
-            .try_into()
-            .expect("hash must be of size 20")
-    }
-
-    pub fn get_peers(&self) -> Vec<SocketAddrV4> {
-        let info_hash = urlencode(&self.get_info_hash());
-
-        let mut url = Url::parse(self.announce).expect("Not a valid announce url");
-        url.set_query(Some(&format!("info_hash={}", info_hash)));
-
-        let payload = tracker::Request {
-            peer_id: "-DE203s-x49Ta1Q*sgGQ",
-            port: 58438,
-            uploaded: 0,
-            downloaded: 0,
-            left: self.info.length,
-            compact: 1,
-        };
-
-        let client = reqwest::blocking::Client::new();
-        let res = match client.get(url).query(&payload).send() {
-            Ok(r) => r.bytes(),
-            Err(e) => panic!("Failed to establish connection to tracker: {}", e),
-        };
-
-        let input = res.unwrap();
-        let de_res = match bendy::serde::from_bytes::<tracker::Response>(&input) {
-            Ok(v) => v,
-            Err(_) => panic!("Failed to deserialize tracker response"),
-        };
-
-        let chunked_peers = de_res.peers.chunks_exact(6);
-        let mut peers: Vec<SocketAddrV4> = Vec::new();
-        for peer in chunked_peers {
-            let ip: [u8; 4] = peer[..4].try_into().unwrap();
-            let ip = Ipv4Addr::from(ip);
-
-            let port: [u8; 2] = peer[4..6].try_into().unwrap();
-            let port = u16::from_be_bytes(port);
-
-            let socket = SocketAddrV4::new(ip, port);
-            peers.push(socket);
-        }
-        peers
-    }
-
-    pub async fn connect_to_peers(&self, peers: Vec<SocketAddrV4>) {
-        for peer in peers {
-            self.establish_connection(peer).await;
-        }
-    }
-
-    async fn establish_connection(&self, peer: SocketAddrV4) {
-        if let Ok(mut stream) = TcpStream::connect(peer) {
-            println!("Connected to peer: {}", peer);
-
-            // Establish the handshake
-            self.establish_handshake(&mut stream);
-
-            // Send intersted & Receive unchocke
-            stream.write(&Message::interested().serialize()).unwrap();
-            let _unchoke = Message::from_stream(stream.try_clone().unwrap());
-
-            // Create the file
-            let mut file = File::create("debian.iso").unwrap();
-            // Split the hashes
-            let piece_hashes = self.info.pieces.chunks(20);
-
-            // Download eache pieces
-            for (index, hash) in piece_hashes.enumerate() {
-                self.download_piece(&mut stream, &mut file, index, hash);
-            }
-        } else {
-            println!("Couldn't connect to peer...");
-        }
-    }
-
-    fn establish_handshake(&self, stream: &mut TcpStream) {
-        // Send an handshake
-        stream
-            .write(&Handshake::new(self.get_info_hash(), "-DE203s-x49Ta1Q*sgGQ").serialize())
+    pub fn establish_handshake(&mut self, peer_id: &str) {
+        // Send handhake message
+        self.stream
+            .write(&Handshake::new(self.metainfo.get_info_hash(), peer_id).serialize())
             .unwrap();
 
-        // Receive handshake
-        let _received_hanshake = Handshake::from_stream(stream.try_clone().unwrap());
+        // Receive handshake response
+        let _received_hanshake = Handshake::from_stream(self.stream.try_clone().unwrap());
 
         // Receive bitfield
-        let _bitfield_message = Message::from_stream(stream.try_clone().unwrap());
-        // let bf = bitfield_message.payload;
+        let _bitfield_message = Message::from_stream(self.stream.try_clone().unwrap());
 
         // Receive unchocke
-        let _unchoke = Message::from_stream(stream.try_clone().unwrap());
+        let _unchoke = Message::from_stream(self.stream.try_clone().unwrap());
     }
 
-    fn download_piece(&self, stream: &mut TcpStream, file: &mut File, index: usize, hash: &[u8]) {
-        // Download one piece
-        let mut vec_piece: Vec<u8> = Vec::new();
+    pub fn download_piece(&mut self, index: usize) {
+        let mut buff: Vec<u8> = Vec::new();
         let mut offset = 0;
+        let hash_list: Vec<&[u8]> = self.metainfo.info.pieces.chunks(20).collect();
 
-        while offset < self.info.piece_length {
-            let length: u32 = 2_u32.pow(14);
+        self.stream.write(
+            &Message::interested().serialize()
+        ).unwrap();
+
+        let _unchoke = Message::from_stream(self.stream.try_clone().unwrap());
+
+        while offset < self.metainfo.info.piece_length {
+            let length = 2_u32.pow(14);
             // Send request
-            stream
+            self.stream
                 .write(&Message::request(index as u32, offset, length).serialize())
                 .unwrap();
 
             // Receive piece
-            let piece = Message::from_stream(stream.try_clone().unwrap());
+            let piece = Message::from_stream(self.stream.try_clone().unwrap());
             let p_index = u32::from_be_bytes(piece.payload.get(0..4).unwrap().try_into().unwrap());
             let p_offset = u32::from_be_bytes(piece.payload.get(4..8).unwrap().try_into().unwrap());
             let p_data = piece.payload.get(8..).unwrap();
-            vec_piece.append(&mut p_data.to_vec());
+            buff.append(&mut p_data.to_vec());
             offset = offset + length;
             println!("Received piece {} at offset {}", p_index, p_offset);
         }
-
-        // Verify the hashs of the downloaded piece
-        let mut hasher = Sha1::new();
-        hasher.update(&vec_piece);
-        let result = hasher.finalize();
-        if result[..] == hash[..] {
-            println!(
-                "Cheksum {} valid for piece {}",
-                hash.into_iter().map(|v| format!("{:x}",v)).collect::<String>(),
-                index,
-            );
-        } else {
-            panic!("Invalid checksum");
+        // Verify the piece checksum
+        if !is_checksum_correct(buff, hash_list[index]) {
+            println!("INVALID CHEKSUM")
         }
-
-        // Write the piece to the file
-        file.write_all(&vec_piece).unwrap();
-
         // Send we have the piece
-        stream
+        self.stream
             .write(&Message::have(index as u32).serialize())
             .unwrap();
     }
+}
+
+fn is_checksum_correct(buff: Vec<u8>, hash: &[u8]) -> bool {
+    let mut hasher = Sha1::new();
+    hasher.update(&buff);
+    let result = hasher.finalize();
+    result[..] == hash[..]
 }
 
 fn _has_piece(bf: &Vec<u8>, index: u32) -> bool {
@@ -213,6 +134,19 @@ async fn test_connecting_to_peers() {
     let data =
         std::fs::read("debian-11.5.0-amd64-netinst.iso.torrent").expect("Unable to read file");
     let metainfo = Metainfo::from_bytes(&data);
-    let peers = [SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 53709)];
-    let _connection = metainfo.connect_to_peers(peers.to_vec()).await;
+    let peer_id = "-DE203s-x49Ta1Q*sgGQ";
+    let peers = [SocketAddrV4::new(
+        std::net::Ipv4Addr::new(127, 0, 0, 1),
+        53709,
+    )];
+
+    let n_piece = metainfo.info.pieces.chunks(20).len();
+
+    for peer in peers {
+        let mut peer = Peer::establish_connection(peer, metainfo.clone()).unwrap();
+        peer.establish_handshake(peer_id);
+        for piece in 0..n_piece {
+            peer.download_piece(piece);
+        }
+    }
 }
